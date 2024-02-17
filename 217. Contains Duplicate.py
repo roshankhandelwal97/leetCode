ecdf8325ddae052a354be671a83e97ee640b1d63
roshankhandelwal97 <112270284+roshankhandelwal97@users.noreply.github.com>
2024-02-16 20:39:25 -0500

    Create 217. Contains Duplicate.py

---
+++ 217. Contains Duplicate.py
@@ -0,0 +1,18 @@
+#https://leetcode.com/problems/contains-duplicate/description/
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        dic = {}
+
+        for i in range(len(nums)):
+            if nums[i] in dic: 
+                return True
+            else: 
+                dic[nums[i]] = 1
+        return False
+
+
+        
