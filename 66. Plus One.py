f2ec94c2280870d6f9304d82bc9eb04e66f43651
roshankhandelwal97 <112270284+roshankhandelwal97@users.noreply.github.com>
2024-05-19 00:01:45 -0400

    Create 66. Plus One.py

---
+++ 66. Plus One.py
@@ -0,0 +1,11 @@
+#https://leetcode.com/problems/plus-one/description/
+
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in reversed(range(len(digits))):
+            if digits[i] == 9: 
+                digits[i] = 0
+            else:
+                digits[i] += 1
+                return digits
+        return [1] + digits
